d47b38c3c4a17dceb3776a19ad0ea99931432ec2
Jason Fussenegger <boardintheblacksnow@gmail.com>
2018-04-13 17:40:09 -0600

    Updated Android app to send animations. Added SerialUsbAnimation.ino to work with Android app

--- Android/TotemLed/app/src/main/java/com/edc/totemled/MainActivity.java
+++ Android/TotemLed/app/src/main/java/com/edc/totemled/MainActivity.java
@@ -1,5 +1,6 @@
 package com.edc.totemled;
 
+import android.os.Handler;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -26,28 +27,48 @@ import java.util.Map;
 
 public class MainActivity extends Activity {
     public final String ACTION_USB_PERMISSION = "com.edc.totemled.USB_PERMISSION";
-    public final int LED_COUNT = 30;
-    Button startButton, sendButton, clearButton, stopButton, rainbowTrailButton;
+    public final int NUMLINES = 7;
+    public final int NUMPIXELS = 30;
+    public final int NUMCOLORS = 3;
+    Button startButton, sendButton, clearButton, stopButton;
+    Button rgbTrailButton, rainbowTrailButton, rainbowCycleButton;
     TextView textView;
     EditText editText;
     UsbManager usbManager;
     UsbDevice device;
     UsbSerialDevice serialPort;
     UsbDeviceConnection connection;
+    public int numFrames;
+    public int currentFrame;
+    byte [][][][] animation;
 
-    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
-        @Override
+//    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
+//        @Override
+//        public void onReceivedData(byte[] arg0) {
+//            String data = null;
+//            try {
+//                data = new String(arg0, "UTF-8");
+//                data.concat("/n");
+//                tvAppend(textView, data);
+//            } catch (UnsupportedEncodingException e) {
+//                e.printStackTrace();
+//            }
+//
+//
+//        }
+//    };
+    UsbSerialInterface.UsbReadCallback testCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
+                @Override
         public void onReceivedData(byte[] arg0) {
             String data = null;
             try {
-                data = new String(arg0, "UTF-8");
-                data.concat("/n");
-                tvAppend(textView, data);
-            } catch (UnsupportedEncodingException e) {
+                if (arg0[0] == 0x00) {
+                    currentFrame++;
+                    SendFrame(currentFrame);
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-
-
         }
     };
     private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
@@ -61,12 +82,12 @@ public class MainActivity extends Activity {
                     if (serialPort != null) {
                         if (serialPort.open()) { //Set Serial Connection Parameters.
                             setUiEnabled(true);
-                            serialPort.setBaudRate(9600);
+                            serialPort.setBaudRate(115200);
                             serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
                             serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
                             serialPort.setParity(UsbSerialInterface.PARITY_NONE);
                             serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
-                            serialPort.read(mCallback);
+                            serialPort.read(testCallback);
                             tvAppend(textView,"Serial Connection Opened!\n");
 
                         } else {
@@ -98,7 +119,9 @@ public class MainActivity extends Activity {
         sendButton = (Button) findViewById(R.id.buttonSend);
         clearButton = (Button) findViewById(R.id.buttonClear);
         stopButton = (Button) findViewById(R.id.buttonStop);
+        rgbTrailButton = (Button) findViewById(R.id.rgbTrailButton);
         rainbowTrailButton = (Button) findViewById(R.id.rainbowTrailButton);
+        rainbowCycleButton = (Button) findViewById(R.id.rainbowCycleButton);
         editText = (EditText) findViewById(R.id.editText);
         textView = (TextView) findViewById(R.id.textView);
         textView.setMovementMethod(new ScrollingMovementMethod());
@@ -116,7 +139,9 @@ public class MainActivity extends Activity {
         startButton.setEnabled(!bool);
         sendButton.setEnabled(bool);
         stopButton.setEnabled(bool);
+        rgbTrailButton.setEnabled(bool);
         rainbowTrailButton.setEnabled(bool);
+        rainbowCycleButton.setEnabled(bool);
         textView.setEnabled(bool);
 
     }
@@ -167,40 +192,132 @@ public class MainActivity extends Activity {
         textView.scrollTo(0,0);
     }
 
-    public void onClickRainbowTrail(View view) {
-        int red = 0;
-        int green = 0;
-        int blue = 0;
+    /**
+     * Setup to initialize variables for new animation
+     * @param frameCount total number of frames that will be rendered
+     */
+    public void AnimationSetup(int frameCount) {
+        currentFrame = 0;
+        numFrames = frameCount;
+        animation = new byte[numFrames][NUMLINES][NUMPIXELS][NUMCOLORS];
+    }
 
-        int nextRed = 0;
-        int nextGreen = 0;
-        int nextBlue = 0;
+    public void SendFrame(int frameSlice)
+    {
+        byte[] frameByte = TransformToOneDimensionalArray(frameSlice);
+        //Console.WriteLine(frameByte.Length);
+        byte[] header = new byte[] { 0x01 };//, (byte)(test.Length/3)};
+        byte[] footer = new byte[] { 0x00 };
 
-        byte[] nextByte = new byte[3];
+        //Console.WriteLine("writing data");
+        //tvAppend(textView, "Writing data - frame" + currentFrame + "\n");
+        serialPort.write(header);
+        serialPort.write(frameByte);
+        serialPort.write(footer);
+        //serialPort.ReadByte();
+    }
 
-        for (int i = 0; i < LED_COUNT; i++) {
-            if (red < 255) {
-                nextRed = red + 26;
-                red = Math.min(nextRed, 255);
-                nextByte[0] = (byte)red;
-                nextByte[1] = 0;
-                nextByte[2] = 0;
-            } else if (green < 255) {
-                nextGreen = green + 26;
-                green = Math.min(nextGreen, 255);
-                nextByte[0] = 0;
-                nextByte[1] = (byte)green;
-                nextByte[2] = 0;
-            } else if (blue < 255) {
-                nextBlue = blue + 26;
-                blue = Math.min(nextBlue, 255);
-                nextByte[0] = 0;
-                nextByte[1] = 0;
-                nextByte[2] = (byte) blue;
+    private byte[] TransformToOneDimensionalArray(int frameSlice)
+    {
+        byte[] frameByte = new byte[NUMLINES * NUMPIXELS * NUMCOLORS];
+        int iterator = 0;
+        for (int line = 0; line < NUMLINES; line++)
+        {
+            for (int pixel = 0; pixel < NUMPIXELS; pixel++)
+            {
+                for (int color = 0; color < NUMCOLORS; color++)
+                {
+                    //                        Console.WriteLine(iterator);
+                    frameByte[iterator] = animation[frameSlice][line][pixel][color];
+                    iterator++;
+                }
             }
-            serialPort.write(nextByte);
+        }
+        return frameByte;
+    }
 
-            //tvAppend(textView, "\nData Sent : " + (0xff&nextByte[2]) + ", " + (0xff&nextByte[3]) + ", " + (0xff&nextByte[4]) + "\n");
+    public void onClickRgbTrail(View view) {
+        AnimationSetup(60);
+
+        byte[] nextColor;
+
+        for (int frame = 0; frame < numFrames; frame++) {
+            for (int line = 0; line < NUMLINES; line++) {
+                for (int pixel = 0; pixel < NUMPIXELS; pixel++) {
+                    nextColor = new byte[3];
+
+                    int colorCheck = (pixel + frame + line) % 3;
+                    if (colorCheck == 2) {
+                        nextColor[0] = 100;
+                        nextColor[1] = 0;
+                        nextColor[2] = 0;
+                    } else if (colorCheck == 1) {
+                        nextColor[0] = 0;
+                        nextColor[1] = 100;
+                        nextColor[2] = 0;
+                    } else if (colorCheck == 0) {
+                        nextColor[0] = 0;
+                        nextColor[1] = 0;
+                        nextColor[2] = 100;
+                    }
+
+                    animation[frame][line][pixel] = nextColor;
+                }
+            }
+        }
+        SendFrame(currentFrame);
+    }
+
+    public void onClickRainbowTrail(View view) {
+        AnimationSetup(256 / 4);
+
+        int frame, line, pixel;
+
+        for(frame=0; frame < numFrames; frame++) {
+            for (line = 0; line < NUMLINES; line++) {
+                for (pixel = 0; pixel < NUMPIXELS; pixel++) {
+                    byte colorByte = (byte) ((pixel + frame * 4) & 0xFF);
+                    animation[frame][line][pixel] = Wheel(colorByte);
+                }
+            }
+        }
+
+        SendFrame(currentFrame);
+    }
+
+    // Rainbow Cycle Program - Equally distributed
+    public void onClickRainbowTrailCycle(View view) {
+        AnimationSetup(256 / 4);
+
+        int frame, line, pixel;
+
+        for(frame=0; frame < numFrames; frame++) {
+            for (line = 0; line < NUMLINES; line++) {
+                for (pixel = 0; pixel < NUMPIXELS; pixel++) {
+                    animation[frame][line][pixel] = Wheel((byte) (((pixel * 256 / NUMPIXELS) + frame) & 255));
+                }
+            }
+        }
+
+        SendFrame(currentFrame);
+    }
+    // Input a value 0 to 255 to get a color value.
+    // The colours are a transition r - g - b - back to r.
+    byte[] Wheel(byte WheelPos) {
+        if(WheelPos < 85) {
+            byte val1 = (byte)((WheelPos * 3) & 0xFF);
+            byte val2 = (byte)((255 - WheelPos * 3) & 0xFF);
+            return new byte[] {val1, val2, 0};
+        } else if(WheelPos < 170) {
+            WheelPos -= 85;
+            byte val1 = (byte)((WheelPos * 3) & 0xFF);
+            byte val2 = (byte)((255 - WheelPos * 3) & 0xFF);
+            return new byte[] {val2, 0, val1};
+        } else {
+            WheelPos -= 170;
+            byte val1 = (byte)((WheelPos * 3) & 0xFF);
+            byte val2 = (byte)((255 - WheelPos * 3) & 0xFF);
+            return new byte[] {0, val1, val2};
         }
     }
 
